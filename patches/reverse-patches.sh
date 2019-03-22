77f9130f007c673ed43bf652acf0cf897b9df91d
seluce <36071735+seluce@users.noreply.github.com>
2019-03-22 12:23:53 +0100

    fix reverse patches

--- patches/reverse-patches.sh
+++ patches/reverse-patches.sh
@@ -2,13 +2,8 @@
 cd ../../../..
 cd packages/apps/Settings
 git reset --hard && git clean -fd
-git reset --hard && git clean -fd
-git clean -f -d
 cd ../../../
 cd system/core
-git reset --hard && git clean -fd
-git reset --hard && git clean -fd
-git reset --hard && git clean -fd
 git reset --hard && git clean -fd
 cd ../..
 cd system
