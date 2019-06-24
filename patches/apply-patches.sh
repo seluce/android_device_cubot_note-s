6c39035f639c4cd4174c2259c6fde420d1c333f3
seluce <ferris.daehn@outlook.de>
2019-06-24 11:45:21 +0200

    add MicroG support for M

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -17,6 +17,11 @@ cd ..
 cd frameworks/av
 patch -p1 < ../../device/CUBOT/NOTE_S/patches/frameworks_av/0001-frameworks_av.patch
 cd ../..
+cd frameworks/base
+# only use the optional patch for LineageOS and /e/ to fix MicroG signature spoofing
+# patch -p1 < ../../device/CUBOT/NOTE_S/patches/frameworks_base/0001-frameworks_base_signature_spoofing.patch
+# git clean -f -d
+cd ../..
 cd external/wpa_supplicant_8
 patch -p1 < ../../device/CUBOT/NOTE_S/patches/external_wpa_supplicant_8/0001-Ignore-fake-nvram-wifi.patch
 cd ../..
