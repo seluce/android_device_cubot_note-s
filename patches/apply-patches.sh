7b604c73850e62b59c762ccf6bde5cdcb290620d
seluce <ferris.daehn@outlook.de>
2019-06-24 11:20:26 +0200

    fix device name in patches

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -32,7 +32,7 @@ patch -p1 < ../../device/CUBOT/NOTE_S/patches/frameworks_base/0002-frameworks_ba
 patch -p1 < ../../device/CUBOT/NOTE_S/patches/frameworks_base/0003-fix-out-of-memory-gl-crashes.patch
 patch -p1 < ../../device/CUBOT/NOTE_S/patches/frameworks_base/0004-show-carrier-name.patch
 # only use the optional patch for LineageOS and /e/ to fix MicroG signature spoofing
-# patch -p1 < ../../device/CUBOT/Echo/patches/frameworks_base/0005-frameworks_base_signature_spoofing.patch
+# patch -p1 < ../../device/CUBOT/NOTE_S/patches/frameworks_base/0005-frameworks_base_signature_spoofing.patch
 # git clean -f -d
 cd ../..
 cd system/netd
